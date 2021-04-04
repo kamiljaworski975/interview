2e4e3eeec5f3f1fc03101b51c413bd6b56e3ca3f
Kamil <jaworskiebiznes@gmail.com>
2021-04-04 07:23:30 +0100

    project start

---
+++ webpack.config.js
@@ -0,0 +1,121 @@
+/* eslint-disable indent */
+const path = require('path');
+const fs = require('fs');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const environment = require('./configuration/environment');
+
+console.log(path.resolve(environment.paths.source));
+
+module.exports = {
+  entry: {
+    index: path.resolve(environment.paths.source, 'scripts', 'index.js'),
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: environment.paths.output,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+          'postcss-loader',
+          {
+            loader: 'resolve-url-loader',
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|gif|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'images/design/[name].[hash:6].[ext]',
+              limit: environment.limits.images,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'fonts/[name].[hash:6].[ext]',
+              publicPath: '../',
+              limit: environment.limits.fonts,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new ImageMinimizerPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      minimizerOptions: {
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }],
+          [
+            'svgo',
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+    new CleanWebpackPlugin({
+      verbose: true,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(environment.paths.source, 'assets'),
+          to: path.resolve(environment.paths.output, 'assets'),
+          toType: 'dir',
+          noErrorOnMissing: true,
+          globOptions: {
+            ignore: ['*.DS_Store', 'Thumbs.db'],
+          },
+        },
+      ],
+    }),
+    new HTMLWebpackPlugin({
+      inject: 'body',
+      hash: false,
+      filename: 'index.html',
+      template: path.resolve(environment.paths.source, 'index.html'),
+      chunks: ['index'],
+    }),
+  ],
+  target: 'web',
+};
